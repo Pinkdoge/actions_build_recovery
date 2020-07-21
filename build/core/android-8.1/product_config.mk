57a544e3d6b8544a13f339ae52a1c96e84e5ebc3
Pinkdoge <42758990+Pinkdoge@users.noreply.github.com>
2020-07-21 15:56:38 +0800

    fix product

--- build/core/android-8.1/product_config.mk
+++ build/core/android-8.1/product_config.mk
@@ -253,7 +253,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS := $(strip $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_D
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := $(strip $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK))
 
 # Find the device that this product maps to.
-TARGET_DEVICE := kiwi
+TARGET_DEVICE := device_code
 
 # Figure out which resoure configuration options to use for this
 # product.
